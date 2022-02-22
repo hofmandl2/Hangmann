e63ae198f5750693ecc1ef0bee73d3f4a5aa4de7
Gabriel Rendon1 <grendon2002@gmail.com>
2022-02-22 18:22:34 +0100

    arregle el bug de que no se ponian las letras que faltaban, ahora toca terminar que pasa cuando se gana y se pierde, y otros detalles pequenos de diseno

--- Matriz.cs
+++ Matriz.cs
@@ -4,82 +4,72 @@ using System.Threading.Tasks;
     
 class Ahorcado
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    //llamar a las otras clases
+    Logic logic = new Logic();
+  }
+  class Logic 
+  {
+    List<string> wort_input = new List<string>(); //lista para guardas las palabras
+    string[] palabras_array = new string[]{}; //array que tendra las palabras
+    string palabra; //entrada de la palabra
+    char[] palabra_adivinar; //la palabra escogida en tipo char
+    char caracter;
+    bool find = false; //si el caracter se encontro
+    bool complet = false; //si la palabra ya esta completa
+    public Logic()
     {
-        //llamar a las otras clases
-        Logic logic = new Logic();
-    }
-    class Logic 
-    {
-      List<string> wort_input = new List<string>(); //lista para guardas las palabras
-      string[] palabras_array = new string[]{}; //array que tendra las palabras
-      string palabra; //entrada de la palabra
-      char[] palabra_adivinar; //la palabra escogida en tipo char
-      char caracter;
-      bool find = false;
-      public Logic()
+      Console.WriteLine("Geben Sie bitte Worte ab, damit das Spiel anfagen kann");
+      while (true)
       {
-        Console.WriteLine("Geben Sie bitte Worte ab, damit das Spiel anfagen kann");
-        while (true)
-        {
-          Console.Write("Wort: ");
-          palabra = Console.ReadLine();
-          if (palabra == "q"){break;} //Si es q salir
-          else{wort_input.Add(palabra);} //de lo contrario entrar en la list
-        }
+        Console.Write("Wort: ");
+        palabra = Console.ReadLine();
+        if (palabra == "q"){break;} //Si es q salir
+        else{wort_input.Add(palabra);} //de lo contrario entrar en la list
+      }
         
-        palabras_array = wort_input.ToArray(); //se convierte la lista en un array
-        string choosed_wort = palabras_array[random_num_generation()]; //Se guarda la palabra aleatoria en una variable para convertirla en char
-        palabra_adivinar = choosed_wort.ToCharArray(); //se convierte la palabra en un array tipo char
-        char[] lista_vacia = new char[palabra_adivinar.Length]; //lista que se imprimira vacia y se llenara
+      palabras_array = wort_input.ToArray(); //se convierte la lista en un array
+      string choosed_wort = palabras_array[random_num_generation()]; //Se guarda la palabra aleatoria en una variable para convertirla en char
+      palabra_adivinar = choosed_wort.ToCharArray(); //se convierte la palabra en un array tipo char
+      char[] lista_vacia = new char[palabra_adivinar.Length]; //lista que se imprimira vacia y se llenara
 
-        for (int i = 0; i < palabra_adivinar.Length;i++)
+      for (int i = 0; i < palabra_adivinar.Length;i++) //rellena la lista que el usario vera
+      {
+        lista_vacia[i] = '_';
+      }
+      /*foreach(char i in palabra_adivinar)
+      {
+        Console.Write(i+" ");
+      }
+      Console.WriteLine(" ");*/
+      while (true)
+      {
+        Console.WriteLine("\nGeben Sie ein Buschatabe: "); //letra dada 
+        caracter = char.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        for(int i = 0 ; i< palabra_adivinar.Length; i++) //for que vera si la letra dada esta en la lista
         {
-          lista_vacia[i] = '_';
-        }
-        foreach(char i in palabra_adivinar)
-        {
-          Console.Write(i+" ");
-        }
-        Console.WriteLine(" ");
-        while (true)
-        {
-          Console.WriteLine("Geben Sie ein Buschatabe: ");
-          caracter = char.Parse(Console.ReadLine());
           Console.WriteLine();
+          if (caracter == palabra_adivinar[i])
+          {
+            Console.WriteLine("Richtig! ");
+            lista_vacia[i] = caracter;
+            find = true;
+          }
+          else{//tries++;
+          }
 
-          for(int i = 0 ; i< palabra_adivinar.Length; i++)
-          {
-            Console.WriteLine("For");
-            Console.Write("carcter: "+caracter);
-            Console.WriteLine();
-            if (caracter != palabra_adivinar[i])
-            {
-              find = false;
-              //tries++;
-            }
-            else{
-              Console.WriteLine("Richtig! ");
-              lista_vacia[i] = caracter;
-              find = true;
-              break;          
-            }
-            /*Bug, cuando el juego encuentra una palabra que tiene mas letras repetidas, siempre coje la primera, y no remeplaza a la segunda tambien no se peude salir cuando el juego
-            ya se termino*/
-          }
-          if (find == true)
-          {
-            foreach (char i in lista_vacia)
-            {
-              Console.Write(i+" ");
-            }
-          }
-          else
-          {
-            //Draw
-          }
+        }
+
+        if (find != true)
+        {
+          //draw
         }
         
+        find = false;
+
         foreach (char i in lista_vacia)
         {
           
@@ -88,16 +78,18 @@ class Ahorcado
         }
 
       }
-      private void getArray()
+      Console.Write("\nFuera de while");
+      void getArray()
       {
         Console.WriteLine(String.Join("\n", palabras_array));
       }
-      private int random_num_generation()
+      int random_num_generation()
       {
         Random random = new Random();
         int i = random.Next(palabras_array.Length);
         return i;
       }
     }
+  }
 }
   
