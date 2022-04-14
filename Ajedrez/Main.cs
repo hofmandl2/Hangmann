377e09a13f6d67749f7b56fe411e649e7f20cc08
Gabriel Rendon <gabriel.rendongmx@gmail.com>
2022-04-14 13:06:05 +0200

    Agrege la funcion que devuelve la ficha dicha

--- Ajedrez/Main.cs
+++ Ajedrez/Main.cs
@@ -1,39 +1,55 @@
 using System;
 using System.Collections.Generic;
+using clasePiezas;
 
-namespace Ajedrez
+namespace Ajedrez_main
 {
     class Ajedrez
     {
-        static void Main(string[]arg)
+        
+        static void Main(string[] arg)
         {
-            string[,] tablero = new string[8,8];
-            string reina = " ♔ ";
-            
-            for(int i = 0; i<8; i++)
-            {
-                for(int j = 0; j<8; j++)
-                {
-                    if(i%2 ==0 && j%2 == 0 || i%2 !=0 && j%2 != 0)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.Write(tablero[i,j] = " 1 ");
-                        Console.BackgroundColor = ConsoleColor.Black;    
-                    }
-                    else{
-                    
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.Write(tablero[i,j] = reina);
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                    }
-                    
-                    
-                }
-                Console.WriteLine();
-            }
+            Logic logic = new Logic();
+            logic.start();
         }
-    }
+        class Logic
+        {   
+            Piezas piezas = new Piezas();
+            public void start()
+            {
+                string[,] tablero = new string[8, 8];
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write(piezas.getPiezas("Torre"));
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        else{
+
+                            Console.BackgroundColor = ConsoleColor.Cyan;
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write(piezas.getPiezas("Reina"));
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+                    }
+                    Console.WriteLine();
+                }
+               
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            /*public void start()
+            {
+                
+            }*/
+                
+        }
+
+    } 
 }
---
+++ Ajedrez/piezas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace clasePiezas
+{
+	public class Piezas
+	{     
+		public string getPiezas(string nombre_pieza)
+	    {
+	            	Dictionary<string, string> list_Piezas = list_Piezas = new Dictionary<string, string>()
+	            	{
+	                		{"Rey" , " ♔ "},
+	                		{"Reina" , " ♕ "},
+	                		{"Torre"," ♖ "},
+	                		{"Arfil", " ♗ "},
+	                		{"Caballo", " ♘ "}, 
+	                 		{"Peon", " ♙ "}
+	            	};
+					return list_Piezas[nombre_pieza];	            	
+		}
+	}
+}
