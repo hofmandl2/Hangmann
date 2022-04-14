24d42bc2e96fdb353c14bd9d180f6bb37ca707e7
Gabriel Rendon1 <grendon2002@gmail.com>
2022-04-14 22:47:33 +0200

    se agrego la funcion que rellena al inciar el juego la torre en el trablero

--- Ajedrez/Main.cs
+++ Ajedrez/Main.cs
@@ -10,15 +10,25 @@ namespace Ajedrez_main
         static void Main(string[] arg)
         {
             Logic logic = new Logic();
-            logic.start();
+            //logic.start();
         }
         class Logic
         {   
             Piezas piezas = new Piezas();
-            public void start()
+            public Logic()
             {
                 string[,] tablero = new string[8, 8];
-
+                tablero[0,1] = " 0 ";
+                //relleno el tablero vacio
+                for(int i = 0; i<8; i++)
+                {
+                    for(int j = 0; j<8; j++)
+                    {   
+                        tablero[i,j] = "  ";
+                    }
+                }
+                tablero[7,0] = piezas.getPiezas("Torre");
+                //pintar el tablero
                 for (int i = 0; i < 8; i++)
                 {
                     for (int j = 0; j < 8; j++)
@@ -27,26 +37,55 @@ namespace Ajedrez_main
                         {
                             Console.BackgroundColor = ConsoleColor.Blue;
                             Console.ForegroundColor = ConsoleColor.Black;
-                            Console.Write(piezas.getPiezas("Torre"));
+                            Console.Write(tablero[i,j]);
                             Console.BackgroundColor = ConsoleColor.Black;
                         }
                         else{
 
                             Console.BackgroundColor = ConsoleColor.Cyan;
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write(tablero[i,j]);
+                            /*Console.Write(piezas.getPiezas("Arfil"));
                             Console.Write(piezas.getPiezas("Reina"));
+                            Console.Write(piezas.getPiezas("Caballo"));*/
+
                             Console.BackgroundColor = ConsoleColor.Black;
                             Console.ForegroundColor = ConsoleColor.Black;
                         }
                     }
                     Console.WriteLine();
                 }
-               
+                
                 Console.ForegroundColor = ConsoleColor.White;
+                
             }
             /*public void start()
             {
-                
+                while(true)
+                {
+                    for (int i = 0; i < 8; i++)
+                        {
+                            for (int j = 0; j < 8; j++)
+                            {
+                                if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
+                                {
+                                    Console.BackgroundColor = ConsoleColor.Blue;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Write("  ");
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                }
+                                else{
+
+                                Console.BackgroundColor = ConsoleColor.Cyan;
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.Write("  ");
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.ForegroundColor = ConsoleColor.Black;
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
             }*/
                 
         }
