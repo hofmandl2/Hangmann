fc18a239c781dfb219d978365d9ed3551cfa4f64
Gabriel Rendon <gabriel.rendongmx@gmail.com>
2022-04-22 07:24:24 +0200

    Agrege el metodo que reconoce si es una ficha negro o blanca, tambien cambios en la impresion pero un bug si la pieza se mueve

--- Ajedrez/Main.cs
+++ Ajedrez/Main.cs
@@ -9,31 +9,113 @@ namespace Ajedrez_main
     {
         
        static void Main(string[] arg)
-        {
+       {
             Logic logic = new Logic();
             logic.start();
-        }
-        class Logic
-        {   
-            Piezas piezas = new Piezas();
+       }
+        
+       class Logic
+       {
+        Piezas piezas = new Piezas();
+           
             string[,] tablero = new string[8, 8];
             public Logic()
             {
+                rellenoTableroFichas();                               
+            }
+            public void start()
+            {
                 
+                while(true)
+                {
+                        boardImpress();
+                        Thread.Sleep(2000);
+                }
+            }
+            private void boardImpress()
+            {
+                    Console.Clear();
+                    for (int i = 0; i < 8; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (i % 2 == 0 && j % 2 == 0|| i % 2 != 0 && j % 2 != 0)
+                            {
+                                if(tablero[i,j] == "   ")
+                                {                           
+                                    Console.BackgroundColor = ConsoleColor.Blue;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Write(tablero[i,j]);
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                }
+                                else if(piezas.soyBlanca(i,j) && !piezas.soyNegra(i,j))
+                                { 
+                                    Console.BackgroundColor = ConsoleColor.Blue;
+                                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                    Console.Write(tablero[i,j]);
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                }
+                                else if(piezas.soyNegra(i,j) && !piezas.soyBlanca(i,j))
+                                {                            
+                                    Console.BackgroundColor = ConsoleColor.Blue;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Write(tablero[i,j]);
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                }
+                                
+                                
+                                
+                            }    
+                            else
+                            {
+                                    if(tablero[i,j] == "   ")
+                                    {
+                                        Console.BackgroundColor = ConsoleColor.Cyan;
+                                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                        Console.Write(tablero[i,j]);
+                                        Console.ForegroundColor = ConsoleColor.Black;
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                    }
+                                    else if(piezas.soyNegra(i,j) && ! piezas.soyBlanca(i,j))
+                                    {
+                                        Console.BackgroundColor = ConsoleColor.Cyan;
+                                        Console.ForegroundColor = ConsoleColor.Black;
+                                        Console.Write(tablero[i,j]);
+                                        Console.ForegroundColor = ConsoleColor.Black;
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                
+                                    }
+                                    else if(piezas.soyBlanca(i,j) && !piezas.soyNegra(i,j))
+                                    {
+                                        Console.BackgroundColor = ConsoleColor.Cyan;
+                                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                        Console.Write(tablero[i,j]);
+                                        Console.ForegroundColor = ConsoleColor.Black;
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                    }
+                                    
+                            }
+                                
+                            
+                        }
+                        Console.WriteLine();
+                    }
+            }
+            void rellenoTableroFichas()
+            {
                 tablero[0,1] = " 0 ";
                 //relleno el tablero vacio
                 for(int i = 0; i<8; i++)
                 {
                     for(int j = 0; j<8; j++)
                     {   
-                        tablero[i,j] = "  ";
-                        if(i == 1 && j < 8)
-                        {
-                            tablero[i,j] = piezas.getPiezas("Peon");
-                        }
+                        tablero[i,j] = "   ";
                     }
                 }
-
+                //Fichas negras
                 tablero[piezas.getCoordinat_piezas_negras("TorreIi"),piezas.getCoordinat_piezas_negras("TorreIj")] = piezas.getPiezas("Torre");
                 tablero[piezas.getCoordinat_piezas_negras("ArfilBi"),piezas.getCoordinat_piezas_negras("ArfilBj")] = piezas.getPiezas("Arfil");
                 tablero[piezas.getCoordinat_piezas_negras("Caballo1i"),piezas.getCoordinat_piezas_negras("Caballo1j")] = piezas.getPiezas("Caballo");
@@ -42,54 +124,40 @@ namespace Ajedrez_main
                 tablero[piezas.getCoordinat_piezas_negras("Caballo2i"),piezas.getCoordinat_piezas_negras("Caballo2j")] = piezas.getPiezas("Caballo");
                 tablero[piezas.getCoordinat_piezas_negras("ArfilNi"),piezas.getCoordinat_piezas_negras("ArfilNj")] = piezas.getPiezas("Arfil");
                 tablero[piezas.getCoordinat_piezas_negras("TorreDi"),piezas.getCoordinat_piezas_negras("TorreDj")] = piezas.getPiezas("Torre");
-                
+                //Peon
+                tablero[piezas.getCoordinat_piezas_negras("Peon1i"),piezas.getCoordinat_piezas_negras("Peon1j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon2i"),piezas.getCoordinat_piezas_negras("Peon2j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon3i"),piezas.getCoordinat_piezas_negras("Peon3j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon4i"),piezas.getCoordinat_piezas_negras("Peon4j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon5i"),piezas.getCoordinat_piezas_negras("Peon5j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon6i"),piezas.getCoordinat_piezas_negras("Peon6j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon7i"),piezas.getCoordinat_piezas_negras("Peon7j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_negras("Peon8i"),piezas.getCoordinat_piezas_negras("Peon8j")] = piezas.getPiezas("Peon");
+                //Peon
 
-                
+                //Fichas Negras
+                tablero[piezas.getCoordinat_piezas_blancas("TorreIi"),piezas.getCoordinat_piezas_blancas("TorreIj")] = piezas.getPiezas("Torre");
+                tablero[piezas.getCoordinat_piezas_blancas("ArfilBi"),piezas.getCoordinat_piezas_blancas("ArfilBj")] = piezas.getPiezas("Arfil");
+                tablero[piezas.getCoordinat_piezas_blancas("Caballo1i"),piezas.getCoordinat_piezas_blancas("Caballo1j")] = piezas.getPiezas("Caballo");
+                tablero[piezas.getCoordinat_piezas_blancas("Reyi"),piezas.getCoordinat_piezas_blancas("Reyj")] = piezas.getPiezas("Rey");
+                tablero[piezas.getCoordinat_piezas_blancas("Reinai"),piezas.getCoordinat_piezas_blancas("Reinaj")] = piezas.getPiezas("Reina");
+                tablero[piezas.getCoordinat_piezas_blancas("Caballo2i"),piezas.getCoordinat_piezas_blancas("Caballo2j")] = piezas.getPiezas("Caballo");
+                tablero[piezas.getCoordinat_piezas_blancas("ArfilNi"),piezas.getCoordinat_piezas_blancas("ArfilNj")] = piezas.getPiezas("Arfil");
+                tablero[piezas.getCoordinat_piezas_blancas("TorreDi"),piezas.getCoordinat_piezas_blancas("TorreDj")] = piezas.getPiezas("Torre");
+                //Peon
+                tablero[piezas.getCoordinat_piezas_blancas("Peon1i"),piezas.getCoordinat_piezas_blancas("Peon1j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon2i"),piezas.getCoordinat_piezas_blancas("Peon2j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon3i"),piezas.getCoordinat_piezas_blancas("Peon3j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon4i"),piezas.getCoordinat_piezas_blancas("Peon4j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon5i"),piezas.getCoordinat_piezas_blancas("Peon5j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon6i"),piezas.getCoordinat_piezas_blancas("Peon6j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon7i"),piezas.getCoordinat_piezas_blancas("Peon7j")] = piezas.getPiezas("Peon");
+                tablero[piezas.getCoordinat_piezas_blancas("Peon8i"),piezas.getCoordinat_piezas_blancas("Peon8j")] = piezas.getPiezas("Peon");
+                //Peon
             }
-            public void start()
-            {
-                
-                while(true)
-                {
-                        boardImpress();
-                        Console.WriteLine(piezas.getCoordinat_piezas_negras("Reyi"));
-                        piezas.setCoordinat_piezas_negras("Reyi","Reyj",1,1);
-                        Console.WriteLine(piezas.getCoordinat_piezas_negras("Reyi"));
-                        Thread.Sleep(2000);
-                }
-            }
-            private void boardImpress()
-            {
-                    //Console.Clear();
-                    for (int i = 0; i < 8; i++)
-                    {
-                        for (int j = 0; j < 8; j++)
-                        {
-                            if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
-                            {
-                                Console.BackgroundColor = ConsoleColor.Blue;
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                Console.Write(tablero[i,j]);
-                                Console.BackgroundColor = ConsoleColor.Black;
-                                Console.ForegroundColor = ConsoleColor.Black;
-                            }
-                            else{
-
-                                Console.BackgroundColor = ConsoleColor.Cyan;
-                                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                Console.Write(tablero[i,j]);
-
-                                Console.BackgroundColor = ConsoleColor.Black;
-                                Console.ForegroundColor = ConsoleColor.Black;
-                            }
-                        }
-                        Console.WriteLine();
-                    }
-            }
-        }     
-        
-         
-         
-
-    } 
+          
+       }
+    }
 }
+
+        
--- Ajedrez/piezas.cs
+++ Ajedrez/piezas.cs
@@ -21,19 +21,100 @@ namespace clasePiezas
 		//blancas
 		Dictionary<string, int> coordinaten_blancas = new Dictionary<string, int>()
         {
-                {"Reyi",0},{"Reyj",0},
-                {"Reinai",1},{"Reinaj",1}
-
+                {"TorreIi",7},{"TorreIj",0},
+                {"ArfilBi",7},{"ArfilBj",1},
+				{"Caballo1i",7},{"Caballo1j",2},
+                {"Reyi",7},{"Reyj",3},
+				{"Reinai",7},{"Reinaj",4},
+                {"Caballo2i",7},{"Caballo2j",5},
+				{"ArfilNi",7},{"ArfilNj",6},
+                {"TorreDi",7},{"TorreDj",7},
+				{"Peon1i",6},{"Peon1j",0},
+				{"Peon2i",6},{"Peon2j",1},
+				{"Peon3i",6},{"Peon3j",2},
+				{"Peon4i",6},{"Peon4j",3},
+				{"Peon5i",6},{"Peon5j",4},
+				{"Peon6i",6},{"Peon6j",5},
+				{"Peon7i",6},{"Peon7j",6},
+				{"Peon8i",6},{"Peon8j",7}
         };
 
-		/*public int setCoordinat_piezas_blancas(string clave)
+		public void setCoordinat_piezas_blancas(string cordx,string cordy, int x,int y )
 		{
-			
-		}*/
+			coordinaten_blancas[cordx] = x;
+			coordinaten_blancas[cordy] = y;
+		}
 		public int getCoordinat_piezas_blancas(string clave)
         {
            
 			return coordinaten_blancas[clave];
+        }
+		public bool soyBlanca(int i, int j)
+		{
+			
+			if(coordinaten_blancas["TorreIi"] == i && coordinaten_blancas["TorreIj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["ArfilBi"] == i && coordinaten_blancas["ArfilBj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["Caballo1i"] == i && coordinaten_blancas["Caballo1j"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["ArfilNi"] == i && coordinaten_blancas["ArfilNj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["TorreDi"] == i && coordinaten_blancas["TorreDj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["Caballo2i"] == i && coordinaten_blancas["Caballo2j"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["Reyi"] == i && coordinaten_blancas["Reyj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_blancas["Reinai"] == i && coordinaten_blancas["Reinaj"] == i)
+			{
+				return true;
+			}
+            //Peones
+			if(coordinaten_blancas["Peon1i"] == i && coordinaten_blancas["Peon1j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon2i"] == i && coordinaten_blancas["Peon2j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon3i"] == i && coordinaten_blancas["Peon3j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon4i"] == i && coordinaten_blancas["Peon4j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon5i"] == i && coordinaten_blancas["Peon5j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon6i"] == i && coordinaten_blancas["Peon6j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_blancas["Peon7i"] == i && coordinaten_blancas["Peon7j"] == i)
+			{
+				return true;
+			}
+
+			return false;
         }
 		//negras
 		Dictionary<string, int> coordinaten_negras = new Dictionary<string, int>()
@@ -45,8 +126,15 @@ namespace clasePiezas
 				{"Reinai",0},{"Reinaj",4},
                 {"Caballo2i",0},{"Caballo2j",5},
 				{"ArfilNi",0},{"ArfilNj",6},
-                {"TorreDi",0},{"TorreDj",7}
-
+                {"TorreDi",0},{"TorreDj",7},
+				{"Peon1i",1},{"Peon1j",0},
+				{"Peon2i",1},{"Peon2j",1},
+				{"Peon3i",1},{"Peon3j",2},
+				{"Peon4i",1},{"Peon4j",3},
+				{"Peon5i",1},{"Peon5j",4},
+				{"Peon6i",1},{"Peon6j",5},
+				{"Peon7i",1},{"Peon7j",6},
+				{"Peon8i",1},{"Peon8j",7}
         };
 		public void setCoordinat_piezas_negras(string cordx,string cordy, int x,int y)
         {
@@ -55,9 +143,78 @@ namespace clasePiezas
         }
 		public int getCoordinat_piezas_negras(string clave)
         {
-           
 			return coordinaten_negras[clave];
         }
+	
+		public bool soyNegra(int i, int j)
+		{
+			if(coordinaten_negras["TorreIi"] == i && coordinaten_negras["TorreIj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["ArfilBi"] == i && coordinaten_negras["ArfilBj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["Caballo1i"] == i && coordinaten_negras["Caballo1j"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["ArfilNi"] == i && coordinaten_negras["ArfilNj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["TorreDi"] == i && coordinaten_negras["TorreDj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["Caballo2i"] == i && coordinaten_negras["Caballo2j"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["Reyi"] == i && coordinaten_negras["Reyj"] == i)
+			{
+				return true;
+            }
+			if(coordinaten_negras["Reinai"] == i && coordinaten_negras["Reinaj"] == i)
+			{
+				return true;
+			}
+            //Peones
+			if(coordinaten_negras["Peon1i"] == i && coordinaten_negras["Peon1j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon2i"] == i && coordinaten_negras["Peon2j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon3i"] == i && coordinaten_negras["Peon3j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon4i"] == i && coordinaten_negras["Peon4j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon5i"] == i && coordinaten_negras["Peon5j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon6i"] == i && coordinaten_negras["Peon6j"] == i)
+			{
+				return true;
+			}
+			if(coordinaten_negras["Peon1i"] == i && coordinaten_negras["Peon1j"] == i)
+			{
+				return true;
+			}
+
+			return false;
+			
+        }
+
+
 	}
 
 }
